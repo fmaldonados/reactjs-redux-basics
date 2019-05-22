5fa86d3620b745eddcac8615f6d982c8b05bac15
felix-maldonado <felix.maldonado@laureate.net>
2019-05-22 16:31:11 -0600

    5to video

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import {render} from "react-dom";
 
 import { User } from './components/User';
@@ -28,4 +28,78 @@ class App extends React.Component {
     }
 }
 
-render(<App />, window.document.getElementById('app'));
+render(<App />, window.document.getElementById('app')); */
+
+import { createStore, combineReducers } from "redux";
+
+const mathReducer = (state = {
+    result: 1,
+    lastValues: []
+}, action) => {
+    switch (action.type) {
+        case "ADD":
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
+        case "SUBTRACT":
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
+    }
+    return state
+};
+
+const userReducer = (state = {
+    name: "Félix",
+    age: 27
+}, action) => {
+    switch (action.type) {
+        case "SET_NAME":
+            state = {
+                ...state,
+                name: action.payload
+            };
+            break;
+        case "SET_AGE":
+            state = {
+                ...state,
+                age: action.payload
+            };
+            break;
+    }
+    return state
+};
+
+
+const store = createStore(combineReducers({mathReducer, userReducer}));
+
+store.subscribe(() => {
+    console.log("Store updated! ", store.getState());
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 22
+});
+
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 80
+});
+
+store.dispatch({
+    type: "SET_AGE",
+    payload: 21
+});
+
